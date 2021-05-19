4296df535c90c388fa70ccf2de9232cb2175dabf
AliakbarSu <aliakbar.su@hotmail.com>
2021-05-19 23:25:49 +1200

    add build folder

---
+++ docs/precache-manifest.8fa1371467230d8ffde8044ef6bd19ab.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dbcd92c6ac994fc13d6f",
+    "url": "/css/app.00cab8ca.css"
+  },
+  {
+    "revision": "e0f5b90e5b9ee989e54d27e5689f1789",
+    "url": "/img/classic-tee.e0f5b90e.jpg"
+  },
+  {
+    "revision": "977ba3424919de267e66294fdfc611a7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dbcd92c6ac994fc13d6f",
+    "url": "/js/app.0a55b922.js"
+  },
+  {
+    "revision": "7e51fd8d78abdfc135dd",
+    "url": "/js/chunk-vendors.6f8e289e.js"
+  },
+  {
+    "revision": "4dd2751dfa44c648270334e74aa5aa10",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
